793a8d6d6ae636eb8698986d3b7ef551be6d9f88
Metalmastery <metalmastery@gmail.com>
2013-01-08 20:14:00 +0200

    ...

--- js/fifteens.js
+++ js/fifteens.js
@@ -1,14 +1,13 @@
 function r(f){/in/.test(document.readyState)?setTimeout('r('+f+')',9):f()}
 r(function(){
 	window.logger = true;
-	var f = new fifteens();
+	var f = new Fifteens();
 	f.init();
 
 });
 
-function fifteens(){
+function Fifteens(){
 	if (arguments.callee.instance) {
-		cns('already created');
 		return arguments.callee.instance;
 	}
 	arguments.callee.instance = this;
@@ -34,20 +33,20 @@ function fifteens(){
 				chips[i].show(ctx);
 			}
 		}
-	}
+	};
 
 	var redrawField = function(time){
 		fps = time-oldtime;
 		oldtime = time;
 		if (fps > maxTimeBetweenFrames) maxTimeBetweenFrames = fps;
-		inputs.fps.value =  (1000 / maxTimeBetweenFrames) >> 0;
+		inputs.fps.value = (1000 / fps) >> 0;
 		for (var i in chips) {
 			if (chips[i]) {
 				chips[i].animate(ctx)
 			}
 		}
 		mozRequestAnimationFrame(arguments.callee);
-	}
+	};
 
 	var mouseClickEvent = function(e){
 		var box = canva.getBoundingClientRect();
@@ -56,7 +55,7 @@ function fifteens(){
 		for (var i in chips) {
 			if (chips[i] && chips[i].mouseClick(x,y)) break;
 		}
-	}
+	};
 
 	var altMouseMoveEvent = function(e){
 		var box = canva.getBoundingClientRect()
@@ -73,43 +72,26 @@ function fifteens(){
 				hoverChip = chips[i];
 			}
 		}
-
-		/*if (hoverChip != null) {
-			if (hoverChip != activeChip) {
-				activeChip.hover = false;
-				activeChip.needRedraw = true;
-
-				hoverChip.hover = true;
-				hoverChip.needRedraw = true;
-				activeChip = hoverChip;
-			}
-		} else {
-			activeChip.hover = false;
-			activeChip.needRedraw = true;
-			activeChip = {};
-		}*/
-	}
+	};
 
 	var initChips = function(){
 		for (var i = 0; i<15; i++){
 			chips[i] = new chip((i % 4) * 100, (i / 4 >> 0) * 100, 100, 100);
 		}
 		chips[i] = null;
-	}
+	};
 
 	var bindEvents = function(){
 		canva.onmousemove = function(e){
-			//mouseMoveEvent(e);
 			altMouseMoveEvent(e);
-		}
+		};
 		canva.onclick = function(e){
 			mouseClickEvent(e);
-		}
+		};
 		canva.onmouseout= function(e){
-			//mouseMoveEvent(e);
 			/*TODO realize the mouseout event field reaction*/
 		}
-	}
+	};
 
 	var initCanvas = function(){
 		canva = document.getElementById('canva');
@@ -121,9 +103,9 @@ function fifteens(){
 			return false;
 		}
 		return true;
-	}
+	};
 
-	var chip = function(x,y,width,height){
+	var chip = function(x, y, width, height, color, hoverColor){
 		if (arguments.callee.number != null){
 			arguments.callee.number++;
 			this.number = arguments.callee.number;
@@ -135,32 +117,29 @@ function fifteens(){
 
 		this.x = x || 0;
 		this.y = y || 0;
-		this.width = width || 100
-		this.height = height || 100
-
+		this.width = width || 100;
+		this.height = height || 100;
+		this.color = color || '#66cc88';
+		this.hoverColor = hoverColor || '#ffaaaa';
+		this.animationFrame = 0;
+		this.animation = false;
 		this.needRedraw = false;
 		this.hover = false;
 		this.targetX = null;
 		this.targetY = null;
-	}
+
+	};
 	chip.prototype.animate = function(context){
-		//if (this.targetX != this.x || this.targetY != this.y)
 		if (this.animation)
 		{
-			cns('animate ' + this.number);
-			var trans = 2;
+			//var trans = 2;
+			var trans = this.easing(10);
+			//cns(trans);
 			this.hide(context);
-			/*if (this.x != this.targetX || this.y != this.targetY){
-				this.x += ((this.x > this.targetX) ? (-1) : 1);
-				this.y += ((this.y > this.targetY) ? (-1) : 1)
-			} else {
-				this.targetX = null;
-				this.targetY = null;
-			}*/
 			(this.targetX != null && this.x != this.targetX) ? this.x += ((this.x > this.targetX) ? (-trans) : trans) : this.targetX = null;
 			(this.targetY != null && this.y != this.targetY) ? this.y += ((this.y > this.targetY) ? (-trans) : trans) : this.targetY = null;
 
-			if (!this.targetX && !this.targetY) {
+			if (this.targetX === null && this.targetY === null) {
 				this.animation = false;
 				this.needRedraw = false;
 			}
@@ -169,16 +148,23 @@ function fifteens(){
 		} else if (this.needRedraw) {
 			this.show(context);
 			this.needRedraw = false;
-			cns('redraw ' + this.number);
 		}
 		return false;
-	}
+	};
 	chip.prototype.hide = function(context){
-		context.clearRect(this.x,this.y,this.height,this.width);
-	}
+        context.fillStyle="#ffffff";
+        var radius = 10;
+        var marg = 4;
+        context.beginPath();
+        context.moveTo(this.x+radius+marg,this.y+marg);
+        context.arcTo(this.x+this.width-marg,this.y+marg,this.x+this.width-marg,this.y+radius+marg,radius);
+        context.arcTo(this.x+this.width-marg,this.y+this.height-marg,this.x+this.width-radius-marg,this.y+this.height-marg,radius);
+        context.arcTo(this.x+marg,this.y+this.height-marg,this.x+marg,this.y+this.height-radius-marg,radius);
+        context.arcTo(this.x+marg,this.y+marg,this.x+radius+marg,this.y+marg,radius);
+        context.fill();
+	};
 	chip.prototype.show = function(context){
-		this.hover ? context.fillStyle="#ff0000" : context.fillStyle="#00ff00";
-		//context.fillRect(this.x+1,this.y+1,this.height-2,this.width-2);
+		this.hover ? context.fillStyle = this.hoverColor : context.fillStyle = this.color;
 		var radius = 10;
 		var marg = 5;
 		context.beginPath();
@@ -189,12 +175,12 @@ function fifteens(){
 		context.arcTo(this.x+marg,this.y+marg,this.x+radius+marg,this.y+marg,radius);
 		context.fill();
 		context.stroke();
-		context.fillStyle="#000000"
+		context.fillStyle="#000000";
 		context.textBaseline = "center";
 		context.textAlign = "center";
 		context.font = "italic " + this.width/4 + "pt Arial";
 		context.fillText(this.number, this.x + this.width/2, this.y + this.height/2);
-	}
+	};
 	chip.prototype.mouseOver = function(x,y){
 		var radius = 10;
 		var marg = 5;
@@ -216,7 +202,7 @@ function fifteens(){
 			this.needRedraw = true;
 		}
 		return false;
-	}
+	};
 	chip.prototype.mouseClick = function(x,y){
 		var radius = 10;
 		var marg = 5;
@@ -232,9 +218,6 @@ function fifteens(){
 			if (Math.abs(chips[_this].x - empty%4*this.width) + Math.abs(chips[_this].y - (empty/4>>0)*this.width) < 200){
 				this.targetX = empty % 4 * this.width;
 				this.targetY = ((empty / 4) >> 0) * this.width;
-				cns('_this= ' + _this + ' empty= ' + empty + ' this.number= ' + this.number);
-				cns('targetX= ' + this.targetX + ' targetY= ' + this.targetY);
-				cns((chips[_this].x - empty%4*this.width) + (chips[_this].y - (empty/4>>0)*this.width));
 				this.needRedraw = true;
 				this.animation = true;
 				var tmp = chips[_this];
@@ -244,7 +227,19 @@ function fifteens(){
 			}
 		}
 		return false;
-	}
+	};
+    chip.prototype.background = null;
+	chip.prototype.easing = function(d) {
+		//return c * t / d;
+		this.animationFrame++;
+		if (this.animationFrame > d){
+			this.animationFrame = 0;
+			cns ('end animation');
+			return -1;
+		}
+		else return Math.round(Math.pow((this.animationFrame / d), 5) * this.width - Math.pow(((this.animationFrame-1) / d), 5) * this.width);
+	};
+
 
 
 	this.init = function(){
@@ -253,7 +248,7 @@ function fifteens(){
 		initChips();
 		initField();
 		redrawField();
-	}
+	};
 
 	return this;
 }
